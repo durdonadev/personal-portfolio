2a6a468e3aa9758eac1edc1254412309a035dee6
Durdona Ibragimova <durdona.dev@gmail.com>
2024-03-03 20:49:30 -0500

    Create Input Component

---
+++ src/design-system/Input/Input.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+import { sizeClassNames, shapeClassNames } from "./clasnames";
+
+import { Label } from "../Label";
+import { InputProps } from "./types";
+import "./Input.css";
+import { trimWhiteSpaces } from "../utils";
+
+const Input: React.FC<InputProps> = (props) => {
+    const {
+        type,
+        error,
+        disabled,
+        placeholder,
+        shape,
+        size,
+        hintMessage,
+        labelText,
+        className,
+        id,
+        onChange,
+        value
+    } = props;
+
+    const sizeClassName = size !== undefined ? sizeClassNames[size] : "";
+
+    const shapeClassName = shape !== undefined ? shapeClassNames[shape] : "";
+
+    const errorClassName = error ? "input-error" : "";
+    const textareaClassName = type === "textarea" ? "input-textarea" : "";
+
+    const finalClassNames = trimWhiteSpaces(
+        `input ${sizeClassName} ${shapeClassName} ${errorClassName} ${textareaClassName}`
+    );
+
+    const hintMessageClass = trimWhiteSpaces(
+        `hint-message ${error ? "hint-message--error" : ""}`
+    );
+
+    const handleOnChange = (
+        e:
+            | React.ChangeEvent<HTMLTextAreaElement>
+            | React.ChangeEvent<HTMLInputElement>
+    ) => {
+        onChange(e.target.value);
+    };
+
+    return (
+        <div className={`input-wrapper ${className || ""}`}>
+            {labelText ? (
+                <Label htmlFor={id} disabled={disabled} error={error}>
+                    {labelText}
+                </Label>
+            ) : null}
+            {type === "textarea" ? (
+                <textarea
+                    placeholder={placeholder}
+                    className={finalClassNames}
+                    disabled={disabled}
+                    id={id}
+                    onChange={handleOnChange}
+                    value={value}
+                />
+            ) : (
+                <input
+                    className={finalClassNames}
+                    type={type || "text"}
+                    placeholder={placeholder}
+                    disabled={disabled}
+                    id={id}
+                    onChange={handleOnChange}
+                    value={value}
+                />
+            )}
+
+            {hintMessage ? (
+                <span className={hintMessageClass}>{hintMessage}</span>
+            ) : null}
+        </div>
+    );
+};
+
+export { Input };
