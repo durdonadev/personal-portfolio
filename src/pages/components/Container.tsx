2cf238376de1b227dc5af7bace73373d511f8f63
Durdona Ibragimova <durdona.dev@gmail.com>
2024-03-04 23:38:26 -0500

    Create Container component

---
+++ src/pages/components/Container.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    margin: 0 auto;
+    max-width: 190rem;
+    padding: 2.5rem 36.5rem;
+    background-color: #e5ebf4;
+`;
+
+export { Container };
