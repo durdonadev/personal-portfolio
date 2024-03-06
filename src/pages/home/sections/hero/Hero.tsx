3417227ef8f1add642b0b045898374ee5ecd4111
Durdona Ibragimova <durdona.dev@gmail.com>
2024-03-05 23:20:38 -0500

    Refactor hero text

--- src/pages/home/sections/hero/Hero.tsx
+++ src/pages/home/sections/hero/Hero.tsx
@@ -7,20 +7,18 @@ const BaseContainer = styled(Container)`
     display: flex;
     align-items: center;
     justify-content: space-between;
-    gap: var(--space-50);
 `;
 
 const HeroTextWrapper = styled.div`
-    width: 50%;
+    width: 55%;
 
-    .heading,
-    .description {
-        margin-bottom: var(--space-40);
+    .heading {
+        margin-bottom: var(--space-20);
     }
 
     .description {
-        font-weight: 400;
-        color: var(--primary-500);
+        margin-bottom: var(--space-40);
+        color: var(--jaguar-600);
     }
 `;
 
@@ -28,15 +26,27 @@ const SocialMedia = styled.div`
     display: flex;
     gap: var(--space-10);
     svg {
+        cursor: pointer;
         height: 4rem;
         width: 4rem;
+        transition: all 1s;
+        &:hover {
+            transform: scale(1.3);
+        }
     }
 `;
 
 const HeroImgWrapper = styled.div`
-    width: 50%;
+    width: 42%;
     img {
+        object-fit: contain;
         height: 60.8rem;
+        width: auto;
+        transition: all 1s;
+
+        &:hover {
+            transform: scale(1.1);
+        }
     }
 `;
 
@@ -44,17 +54,20 @@ const Hero = () => {
     return (
         <BaseContainer>
             <HeroTextWrapper>
-                <Typography variant="h1" className="heading">
-                    Hey there, <br></br>It's Durdona
+                {" "}
+                <Typography variant="paragraphLG" className="greeting">
+                    Hi, I am
                 </Typography>
-                <Typography variant="paragraphMD" className="description">
-                    🚀 Passionate Full Stack Developer 🖥️ <br></br>Proficient in
-                    front-end and back-end technologies, dedicated to crafting
-                    exceptional digital experiences.
+                <Typography variant="h1" className="heading">
+                    Durdona Ibragimova
+                </Typography>
+                <Typography variant="h5" weight="bold" className="description">
+                    Full Stack Web Developer
                 </Typography>
                 <SocialMedia>
                     <Icon iconName="linkdin-icon" />
                     <Icon iconName="github-icon" />
+                    <Icon iconName="twitter-icon" />
                 </SocialMedia>
             </HeroTextWrapper>
             <HeroImgWrapper>
